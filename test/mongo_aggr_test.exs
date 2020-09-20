f2e567675e2d9bdace0e6893e1e9bd424980ba75
LINKH <421698843@qq.com>
2020-09-20 08:31:44 -0700

    #Add the correct unit tests

--- test/mongo_aggr_test.exs
+++ test/mongo_aggr_test.exs
@@ -20,47 +20,47 @@ defmodule Mongo.Aggr.Test do
     { :ok, mongo: mongo, db: db, anycoll: anycoll }
   end
 
-  # test "count", ctx do
-  #   if true do
-  #     assert ctx[:anycoll] |> Mongo.Collection.count!(%{value: %{'$gt': 0}}) == 5
-  #   end
-  # end
-
-  # test "distinct", ctx do
-  #   if true do
-  #     assert ctx[:anycoll] |> Mongo.Collection.distinct!("value", %{value: %{"$lt": 3}})
-  #       |> is_list
-  #   end
-  # end
-
-  test "mapreduce", ctx do
+  test "count", ctx do
     if true do
-      anycoll = ctx[:anycoll]
-      assert Mongo.Collection.mr!(anycoll, "function(d){emit(this._id, this.value*2)}", "function(k, vs){return Array.sum(vs)}") |> is_list
-      assert :ok == Mongo.Collection.mr!(anycoll, "function(d){emit('z', 3*this.value)}", "function(k, vs){return Array.sum(vs)}", "somecol")
+      assert ctx[:anycoll] |> Mongo.Collection.count!(%{value: %{'$gt': 0}}) == 5
     end
   end
 
-  test "group", ctx do
+  test "distinct", ctx do
     if true do
-      assert ctx[:anycoll] |> Mongo.Collection.group!(%{a: true}) |> is_list
+      assert ctx[:anycoll] |> Mongo.Collection.distinct!("value", %{value: %{"$lt": 3}})
+        |> is_list
     end
   end
 
-  # test "aggregate", ctx do
+  # test "mapreduce", ctx do
   #   if true do
-  #     assert [%{value: 1}|_] = ctx[:anycoll] |> Mongo.Collection.aggregate([
-  #       %{'$skip': 1},
-  #       %{'$limit': 5},
-  #       %{'$project': %{_id: false, value: true}}
-  #     ], %{cursor: %{}})
+  #     anycoll = ctx[:anycoll]
+  #     assert Mongo.Collection.mr!(anycoll, "function(d){emit(this._id, this.value*2)}", "function(k, vs){return Array.sum(vs)}") |> is_list
+  #     assert :ok == Mongo.Collection.mr!(anycoll, "function(d){emit('z', 3*this.value)}", "function(k, vs){return Array.sum(vs)}", "somecol")
   #   end
   # end
 
-  # test "error count", ctx do
+  # test "group", ctx do
   #   if true do
-  #     assert %Mongo.Error{} = ctx[:anycoll] |> Mongo.Collection.count(%{value: %{'$in': 0}})
+  #     assert ctx[:anycoll] |> Mongo.Collection.group!(%{a: true}) |> is_list
   #   end
   # end
+
+  test "aggregate", ctx do
+    if true do
+      assert [%{value: 1}|_] = ctx[:anycoll] |> Mongo.Collection.aggregate([
+        %{'$skip': 1},
+        %{'$limit': 5},
+        %{'$project': %{_id: false, value: true}}
+      ], %{cursor: %{}})
+    end
+  end
+
+  test "error count", ctx do
+    if true do
+      assert %Mongo.Error{} = ctx[:anycoll] |> Mongo.Collection.count(%{value: %{'$in': 0}})
+    end
+  end
 
 end
--- test/mongo_db_test.exs
+++ test/mongo_db_test.exs
@@ -1,21 +1,21 @@
-# defmodule Mongo.Db.Test do
-#   use ExUnit.Case, async: false
+defmodule Mongo.Db.Test do
+  use ExUnit.Case, async: false
 
-#   test "drop database" do
-#     mongo = Mongo.connect!
-#     db = Mongo.db(mongo, "test_drop")
-#     anycoll = Mongo.Db.collection(db, "coll_db")
-#     Mongo.Collection.drop anycoll
-#     [
-#         %{a: 0, value: 0},
-#         %{a: 1, value: 1},
-#         %{a: 2, value: 1},
-#         %{a: 3, value: 1},
-#         %{a: 4, value: 1},
-#         %{a: 5, value: 3} ] |> Mongo.Collection.insert(anycoll)
+  test "drop database" do
+    mongo = Mongo.connect!
+    db = Mongo.db(mongo, "test_drop")
+    anycoll = Mongo.Db.collection(db, "coll_db")
+    Mongo.Collection.drop anycoll
+    [
+        %{a: 0, value: 0},
+        %{a: 1, value: 1},
+        %{a: 2, value: 1},
+        %{a: 3, value: 1},
+        %{a: 4, value: 1},
+        %{a: 5, value: 3} ] |> Mongo.Collection.insert(anycoll)
 
-#     assert %{dropped: "test_drop", ok: 1.0} = Mongo.Db.dropDatabase!(db)
+    assert %{dropped: "test_drop", ok: 1.0} = Mongo.Db.dropDatabase!(db)
 
-#     assert anycoll |> Mongo.Collection.find |> Enum.count == 0
-#   end
-# end
+    assert anycoll |> Mongo.Collection.find |> Enum.count == 0
+  end
+end
